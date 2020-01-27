e4719c8805377b74318ac57637d24b63157007a4
Hlumisa Mazomba <hmazomba@live.co.za>
2020-01-27 18:17:30 +0200

    Setup api-gateway

---
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -0,0 +1,11 @@
+const listingsResolver =async () => {
+    return[
+        {
+            description: "Yebo",
+            id:1,
+            title: "Test"
+        }
+    ];
+};
+
+export default listingsResolver;
---
+++ api-gateway/src/helpers/accessEnv.js
@@ -0,0 +1,21 @@
+//access a variable inside of process.env
+//caching the values improves performance
+
+
+const cache = {
+
+};
+
+const accessEnv = (key, defaultValue) => {
+    if(!(key in process.env)){
+        if(defaultValue) return defaultValue;
+        throw new Error(`${key} not found in process.env`);
+
+    }
+    if(cache[key]) return cache[key];
+
+    cache[key] = process.env[key];
+    return process.env[key];
+};
+
+export default accessEnv;
